4d4923154ffc6202e6e158ea5b9fd1eceba00388
ghielback-coder <ghielback@gmail.com>
2026-05-23 19:15:54 -0400

    Create CardEntity.kt

---
+++ app/src/main/java/com/mylescaner/app/CardEntity.kt
@@ -0,0 +1,14 @@
+package com.mylescaner.app
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "coleccion")
+data class CardEntity(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val nombreDetectado: String,
+    val fotoUri: String,
+    val fecha: Long = System.currentTimeMillis(),
+    val codigoEdicion: String? = null, // Esto lo llenas después en la colección
+    val edicionSeleccionada: String? = null
+)
