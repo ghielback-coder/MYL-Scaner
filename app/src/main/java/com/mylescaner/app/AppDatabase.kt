b58980beb1ee527e239455702bac55716cb8de57
ghielback-coder <ghielback@gmail.com>
2026-05-24 11:08:41 -0400

    Update AppDatabase.kt

--- app/src/main/java/com/mylescaner/app/AppDatabase.kt
+++ app/src/main/java/com/mylescaner/app/AppDatabase.kt
@@ -15,14 +15,14 @@ abstract class AppDatabase : RoomDatabase() {
         private var INSTANCE: AppDatabase? = null
 
         fun getDatabase(context: Context): AppDatabase {
-            return INSTANCE?: synchronized(this) {
+            return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
-                    "myl_database"
+                    "myl_scanner_db"
                 )
-               .fallbackToDestructiveMigration()
-               .build()
+                .fallbackToDestructiveMigration() // Borra y recrea la BD si cambia la versión
+                .build()
                 INSTANCE = instance
                 instance
             }
