4522821d94f234e9b8d22acda96042478973cf36
ghielback-coder <ghielback@gmail.com>
2026-05-24 11:13:51 -0400

    Update CardEntity.kt

--- app/src/main/java/com/mylescaner/app/CardEntity.kt
+++ app/src/main/java/com/mylescaner/app/CardEntity.kt
@@ -3,13 +3,13 @@ package com.mylescaner.app
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "cartas")
+@Entity(tableName = "cards") // IMPORTANTE: Le pongo nombre fijo a la tabla
 data class CardEntity(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val nombreDetectado: String,
     val fotoUri: String,
+    val edicionSeleccionada: String,
+    val numeroColeccionista: String?,
     val fechaRegistro: Long = System.currentTimeMillis(),
-    val edicionSeleccionada: String = "Sin edición",
-    val numeroColeccionista: String? = null,
-    val enColeccion: Boolean = true // NUEVO: true = la tengo, false = faltante
+    val enColeccion: Boolean = true
 )
