5450fb0a8703aab2a83bd4ef41c7b1e701b7431b
ghielback-coder <ghielback@gmail.com>
2026-05-23 21:21:22 -0400

    Update AppDatabase.kt

--- app/src/main/java/com/mylescaner/app/AppDatabase.kt
+++ app/src/main/java/com/mylescaner/app/AppDatabase.kt
@@ -5,20 +5,23 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [CardEntity::class], version = 1, exportSchema = false)
+@Database(entities = [CardEntity::class], version = 2) // OJO: VERSION = 2
 abstract class AppDatabase : RoomDatabase() {
     abstract fun cardDao(): CardDao
 
     companion object {
-        @Volatile private var INSTANCE: AppDatabase? = null
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
 
         fun getDatabase(context: Context): AppDatabase {
-            return INSTANCE?: synchronized(this) {
+            return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
                     "myl_database"
-                ).build()
+                )
+                .fallbackToDestructiveMigration() // ESTO BORRA LA DB VIEJA
+                .build()
                 INSTANCE = instance
                 instance
             }
